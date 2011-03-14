a8d54eefc78d6d455ca8c7a7050aae194c464a49
techxplorer <corey@techxplorer.com>
2011-03-14 13:50:25 +1030

    initial source code commit

---
+++ src/org/servalproject/mappingservices/MappingServices.java
@@ -0,0 +1,13 @@
+package org.servalproject.mappingservices;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MappingServices extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
