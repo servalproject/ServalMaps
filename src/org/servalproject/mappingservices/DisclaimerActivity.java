4cb7c88d2c4393828392f82282b7922aff438ef8
techxplorer <corey@techxplorer.com>
2011-06-14 21:34:15 +0930

    update the DisclaimerActivity to check for map data files as part of startup routine

--- src/org/servalproject/mappingservices/DisclaimerActivity.java
+++ src/org/servalproject/mappingservices/DisclaimerActivity.java
@@ -17,20 +17,38 @@
  */
 package org.servalproject.mappingservices;
 
+import org.servalproject.mappingservices.services.MapDataService;
+
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class DisclaimerActivity extends Activity implements OnClickListener {
 	
 	/*
 	 * private class level constants
 	 */
-	private final boolean V_LOG = true;
+//	private final boolean V_LOG = true;
 	private final String TAG = "ServalMaps-DA";
+	
+	/*
+	 * private class level variables
+	 */
+	private Button continueButton;
+	private DisclaimerActivity self;
 	
 	/*
      * Called when the activity is first created
@@ -43,18 +61,21 @@ public class DisclaimerActivity extends Activity implements OnClickListener {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.disclaimer_activity);
         
+        self = this;
+        
         // associate the button with our event listener
-        Button button = (Button)findViewById(R.id.btn_disclaimer_continue);
-        button.setOnClickListener(this);
+        continueButton = (Button)findViewById(R.id.btn_disclaimer_continue);
+        continueButton.setClickable(false);
+        continueButton.setOnClickListener(self);
+        
+        // bind to the service
+        bindService(new Intent(this, MapDataService.class), connection, Context.BIND_AUTO_CREATE);
     }
 	
 	@Override
 	public void onClick(View v) {
 		// determine which button was touched
 		if(v.getId() == R.id.btn_disclaimer_continue) {
-			
-			// check to see if the map data is available
-			
 			
 			// start the service and show the map
 			Intent mServiceIntent = new Intent(this, org.servalproject.mappingservices.services.CoreMappingService.class);
@@ -79,9 +100,77 @@ public class DisclaimerActivity extends Activity implements OnClickListener {
 	    	// shutdown the service
 	    	Intent mServiceIntent = new Intent(this, org.servalproject.mappingservices.services.CoreMappingService.class);
 			stopService(mServiceIntent);
+			
+			unbindService(connection);
 	    	
 	        finish();
 	    }
 	}
+	
+	/*
+     * class to handle the incoming messages from the service
+     */
+    private class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MapDataService.MSG_FILES_AVAILABLE:
+            	Bundle mBundle = msg.getData();
+            	
+            	if(mBundle.getInt("fileCount") > 0) {
+            		// enable the button
+            		continueButton.setClickable(true);
+            	} else {
+            		// show toast and exit
+            		//TODO be gentler about the fact that map data cannot be found
+            		Toast.makeText(self.getApplicationContext(), R.string.disclaimer_no_map_file_error, Toast.LENGTH_LONG).show();
+            		self.onActivityResult(0, 0, null);
+            	}
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+    
+    /*
+     * Messenger object that the service can use to send replies
+     */
+    private final Messenger messenger = new Messenger(new IncomingHandler());
+    
+    /*
+     * ServiceConnection class that represents a collection to the MappindDataService
+     */
+    private ServiceConnection connection = new ServiceConnection() {
+    	/*
+    	 * (non-Javadoc)
+    	 * @see android.content.ServiceConnection#onServiceConnected(android.content.ComponentName, android.os.IBinder)
+    	 */
+    	@Override
+    	public void onServiceConnected(ComponentName className, IBinder service) {
+    		// called when the connection is made
+    		Messenger mMessenger = new Messenger(service);
+    		
+    		try {
+    			// send a message to the service to see what its status is
+    			Message msg = Message.obtain(null, MapDataService.MSG_FILES_AVAILABLE);
+    			msg.replyTo = messenger;
+    			mMessenger.send(msg);
+
+    		} catch (RemoteException e) {
+    			Log.e(TAG, "unable to send a message to the MapDataService", e);
+    		}
+    		
+    		
+    	}
+
+    	/*
+    	 * (non-Javadoc)
+    	 * @see android.content.ServiceConnection#onServiceDisconnected(android.content.ComponentName)
+    	 */
+    	@Override
+    	public void onServiceDisconnected(ComponentName className) {
+    		
+    	}
+    };
 
 }
