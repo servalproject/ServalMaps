2a7e5ca3c5d57202ea8791ed8e930eed85b466a3
techxplorer <corey@servalproject.org>
2012-03-22 11:31:39 +1030

    tidy imports for DisclaimerActivity

--- src/org/servalproject/maps/DisclaimerActivity.java
+++ src/org/servalproject/maps/DisclaimerActivity.java
@@ -35,7 +35,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
