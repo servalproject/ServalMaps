fb018471a1db10e533f53b66009a4da843b17699
techxplorer <corey@servalproject.org>
2012-05-28 16:47:38 +0930

    remove old Rhizome integration code that will not work with new version of Rhizome

--- src/org/servalproject/maps/rhizome/Rhizome.java
+++ src/org/servalproject/maps/rhizome/Rhizome.java
@@ -20,17 +20,11 @@
 package org.servalproject.maps.rhizome;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.servalproject.maps.R;
 import org.servalproject.maps.utils.FileUtils;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Environment;
-import android.text.TextUtils;
-import android.util.Log;
+
 
 /**
  * used to add a file to the Rhizome repository
@@ -40,7 +34,7 @@ public class Rhizome {
 	/*
 	 * class level constants
 	 */
-	private static final String TAG = "Rhizome";
+	//private static final String TAG = "Rhizome";
 
 	/**
 	 * add a file to the Rhizome repository
@@ -74,47 +68,6 @@ public class Rhizome {
 		mIntent.putExtra("save_manifest", mManifestFile.getAbsolutePath());
 		context.getApplicationContext().startService(mIntent);
 		
-	}
-	
-	/**
-	 * check to see if a file is in Rhizome
-	 * 
-	 * @param fileNname the name of the file to look for
-	 * @return the full path to the file
-	 */
-	public static String checkForFile(Context context, String fileName) throws FileNotFoundException{
-		
-		// check on the parameters
-		if(context == null) {
-			throw new IllegalArgumentException("the context parameter is required");
-		}
-		
-		if(TextUtils.isEmpty(fileName)) {
-			throw new IllegalArgumentException("the file name parameter is required");
-		}
-		
-		// get the rhizome path
-		String mRhizomePath = context.getString(R.string.system_path_rhizome_data);
-		try {
-			String mExternal = Environment.getExternalStorageDirectory().getCanonicalPath();
-			mRhizomePath = mExternal + mRhizomePath;
-		} catch (IOException e) {
-			Log.e(TAG, "unable to determine the full path to the Rhizome data store", e);
-			throw new FileNotFoundException("unable to determine the full path to the Rhizome data store");
-		}
-		
-		// check on the rhizome path
-		if(FileUtils.isDirectoryReadable(mRhizomePath) == false) {
-			Log.e(TAG, "unable to access the rhizome directory: " + mRhizomePath);
-			throw new FileNotFoundException("unable to access the rhizome directory: " + mRhizomePath);
-		}
-		
-		// check to see if the file is available
-		if(FileUtils.isFileReadable(mRhizomePath + fileName) == true) {
-			return mRhizomePath + fileName;
-		} else {
-			throw new FileNotFoundException("unable to find the specified file: " + mRhizomePath + fileName);
-		}
 	}
 	
 	/*
